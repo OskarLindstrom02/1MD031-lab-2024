3702e3e2ff453d790b79732d802ab60ab62c37f5
OskarLindstrom02 <Oskarolindstrom@hotmail.com>
2025-12-01 14:25:33 +0100

    små fix

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,49 +1,58 @@
 <template>
   <div class="burger">
     <h3>{{ burger.name }}</h3>
-    <img v-bind:src="burger.url" v-bind:alt="burger.name" width="300" height="300">
+    <img
+      v-bind:src="burger.url"
+      v-bind:alt="burger.name"
+      width="300"
+      height="300"
+    />
     <p>Ordered: {{ amountOrdered }}</p>
     <div>
       <button v-on:click="increaseOrder">+</button>
       <button v-on:click="decreaseOrder">-</button>
     </div>
     <ul>
-      <li v-if="burger.gluten">Contains <span class="allergen">gluten</span></li>
-      <li v-if="burger.lactose">Contains <span class="allergen">dairy</span></li>
+      <li v-if="burger.gluten">
+        Contains <span class="allergen">gluten</span>
+      </li>
+      <li v-if="burger.lactose">
+        Contains <span class="allergen">dairy</span>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'OneBurger',
+  name: "OneBurger",
   props: {
-    burger: Object
+    burger: Object,
   },
   data: function () {
     return {
       amountOrdered: 0,
-    }
+    };
   },
   methods: {
     increaseOrder: function () {
       this.amountOrdered += 1;
-      this.$emit('orderedBurger', { 
+      this.$emit("orderedBurger", {
         name: this.burger.name,
-        amount: this.amountOrdered 
+        amount: this.amountOrdered,
       });
     },
     decreaseOrder: function () {
       if (this.amountOrdered > 0) {
         this.amountOrdered -= 1;
-        this.$emit('orderedBurger', { 
+        this.$emit("orderedBurger", {
           name: this.burger.name,
-          amount: this.amountOrdered 
+          amount: this.amountOrdered,
         });
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -55,5 +64,7 @@ export default {
   font-weight: bold;
   text-transform: uppercase;
 }
-.burger p {color: white;}
+.burger p {
+  color: white;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,87 +1,129 @@
 <template>
-<div>
+  <div>
     <header>
-        <img src="https://t4.ftcdn.net/jpg/07/78/23/41/360_F_778234167_Wcm57Vq9GivIZLW5Qd3p2OUBQ0JGOXS1.jpg">
-        <h1>Welcome to BurgerHeaven Online</h1>
+      <img
+        src="https://t4.ftcdn.net/jpg/07/78/23/41/360_F_778234167_Wcm57Vq9GivIZLW5Qd3p2OUBQ0JGOXS1.jpg"
+      />
+      <h1>Welcome to BurgerHeaven Online</h1>
     </header>
     <main>
-        <section id="burger-selection">
-            <h2>Select burger</h2>
-            <p>This is where you execute burger selection</p>
-            <div class="burger-wrapper">
-                <Burger v-for="burger in burgers"
-                        v-bind:burger="burger" 
-                        v-bind:key="burger.name"
-                        v-on:orderedBurger="addToOrder($event)"/>
-            </div>
-        </section>
-        
-        <section id="customer-information">
-            <h2>Customer information</h2>
-            <p>This is where you provide necessary information</p>
-            
-            <h3>Delivery information:</h3>
-            
-            <form>
-    <p>
-        <label for="name">Full name</label><br>
-        <input type="text" id="name" v-model="name" required placeholder="First- and Last name">
-        
-    </p>
-    <p>
-        <label for="email">E-mail</label><br>
-        <input type="email" id="email" v-model="email" required placeholder="E-mail address">
-    </p>
-    
-    <p>
-        <label>Gender:</label><br>
-        <input type="radio" id="male" v-model="gender" value="male" checked>
-        <label for="male">Male</label><br>
-        
-        <input type="radio" id="female" v-model="gender" value="female">
-        <label for="female">Female</label><br>
-        
-        <input type="radio" id="no-gender" v-model="gender" value="no-gender">
-        <label for="no-gender">Do not wish to provide</label>
-        {{ gender }} <!-- Lägg till detta för att testa -->
-    </p>
-    <p>
-        <label for="payment">Payment method</label><br>
-        <select id="payment" v-model="payment">
-            <option selected="selected">Card</option>
-            <option>Klarna</option>
-            <option>Pay at pickup</option>
-            <option>Apple pay</option>
-        </select>
-    </p>
-</form>
-        </section>
-       <section id="map-section">
-    <h2>Delivery Map</h2>
-   <div class="map-container" v-on:click="setLocation">
-    <img id="map" src="/img/polacks.jpg" alt="Delivery Map" width="1920" height="1078">
-    <div class="target" v-bind:style="{ left: location.x + 'px', top: location.y + 'px' }">
-        T
-    </div>     
-</div>
-</section>
-        
-        <button type="button" v-on:click="submitOrder">
-          <img src="https://img.myloview.se/fototapeter/check-icon-vector-check-mark-icon-check-list-button-icon-700-194930748.jpg" alt="Submit" width="20" height="20">
+      <section id="burger-selection">
+        <h2>Select burger</h2>
+        <p>This is where you execute burger selection</p>
+        <div class="burger-wrapper">
+          <Burger
+            v-for="burger in burgers"
+            v-bind:burger="burger"
+            v-bind:key="burger.name"
+            v-on:orderedBurger="addToOrder($event)"
+          />
+        </div>
+      </section>
+
+      <section id="customer-information">
+        <h2>Customer information</h2>
+        <p>This is where you provide necessary information</p>
+
+        <h3>Delivery information:</h3>
+
+        <form>
+          <p>
+            <label for="name">Full name</label><br />
+            <input
+              type="text"
+              id="name"
+              v-model="name"
+              required
+              placeholder="First- and Last name"
+            />
+          </p>
+          <p>
+            <label for="email">E-mail</label><br />
+            <input
+              type="email"
+              id="email"
+              v-model="email"
+              required
+              placeholder="E-mail address"
+            />
+          </p>
+
+          <p>
+            <label>Gender:</label><br />
+            <input
+              type="radio"
+              id="male"
+              v-model="gender"
+              value="male"
+              checked
+            />
+            <label for="male">Male</label><br />
+
+            <input type="radio" id="female" v-model="gender" value="female" />
+            <label for="female">Female</label><br />
+
+            <input
+              type="radio"
+              id="no-gender"
+              v-model="gender"
+              value="no-gender"
+            />
+            <label for="no-gender">Do not wish to provide</label>
+            {{ gender }}
+            <!-- Lägg till detta för att testa -->
+          </p>
+          <p>
+            <label for="payment">Payment method</label><br />
+            <select id="payment" v-model="payment">
+              <option selected="selected">Card</option>
+              <option>Klarna</option>
+              <option>Pay at pickup</option>
+              <option>Apple pay</option>
+            </select>
+          </p>
+        </form>
+      </section>
+      <section id="map-section">
+        <h2>Delivery Map</h2>
+        <div class="map-container">
+          <img
+            id="map"
+            src="/img/polacks.jpg"
+            alt="Delivery Map"
+            width="1920"
+            height="1078"
+            @click="setLocation"
+          />
+          <div
+            class="target"
+            :style="{ left: location.x + 'px', top: location.y + 'px' }"
+          >
+            T
+          </div>
+        </div>
+      </section>
+
+      <button type="button" v-on:click="submitOrder">
+        <img
+          src="https://img.myloview.se/fototapeter/check-icon-vector-check-mark-icon-check-list-button-icon-700-194930748.jpg"
+          alt="Submit"
+          width="20"
+          height="20"
+        />
         Place order
-    </button>
+      </button>
     </main>
-    
+
     <footer>
-        <p>Copyright BurgerHeaven Online 2025</p>
+      <p>Copyright BurgerHeaven Online 2025</p>
     </footer>
-</div>
+  </div>
 </template>
 
 <script>
-import menu from '../assets/menu.json'
-import Burger from '../components/OneBurger.vue'
-import io from 'socket.io-client'
+import menu from "../assets/menu.json";
+import Burger from "../components/OneBurger.vue";
+import io from "socket.io-client";
 
 const socket = io("localhost:3000");
 
@@ -96,143 +138,163 @@ function Menultem(name, url, kCal, hasGluten, hasLactose) {
 
 // 2. Create array of burgers (using const since we won't redefine the array)
 const burgers = [
-  new Menultem("Double burger", "https://images.arla.com/recordid/02F90FAA-0208-47E9-A76D3510DDFDA427/smashburger.jpg", 300, false, true),
-  new Menultem("Big burger", "https://images.themodernproper.com/production/posts/2016/ClassicCheeseBurger_9.jpg?w=1200&h=1200&q=60&fm=jpg&fit=crop&dm=1749310239&s=463b18fc3bb51dc5d96e866c848527c4", 400, true, false),
-  new Menultem("Chicken burger", "https://www.recipetineats.com/tachyon/2023/09/Crispy-fried-chicken-burgers_5.jpg", 500, true, true)
+  new Menultem(
+    "Double burger",
+    "https://images.arla.com/recordid/02F90FAA-0208-47E9-A76D3510DDFDA427/smashburger.jpg",
+    300,
+    false,
+    true
+  ),
+  new Menultem(
+    "Big burger",
+    "https://images.themodernproper.com/production/posts/2016/ClassicCheeseBurger_9.jpg?w=1200&h=1200&q=60&fm=jpg&fit=crop&dm=1749310239&s=463b18fc3bb51dc5d96e866c848527c4",
+    400,
+    true,
+    false
+  ),
+  new Menultem(
+    "Chicken burger",
+    "https://www.recipetineats.com/tachyon/2023/09/Crispy-fried-chicken-burgers_5.jpg",
+    500,
+    true,
+    true
+  ),
 ];
 
 // 3. Print the array to console
 console.log(burgers);
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Burger
+    Burger,
   },
   data: function () {
-  return {
-    burgers: menu,
-    name: '',
-    email: '',
-    gender: 'male',
-    payment: 'Card',
-    orderedBurgers: {},
-    location: { x: 0, y: 0 }
-  }
-},
+    return {
+      burgers: menu,
+      name: "",
+      email: "",
+      gender: "male",
+      payment: "Card",
+      orderedBurgers: {},
+      location: { x: 0, y: 0 },
+    };
+  },
   methods: {
-  getOrderNumber: function () {
-    return Math.floor(Math.random()*100000);
-  },
-  addOrder: function (event) {
-    var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                  y: event.currentTarget.getBoundingClientRect().top};
-    this.location.x = event.clientX - 10 - offset.x;
-    this.location.y = event.clientY - 10 - offset.y;
-  },
-  submitOrder: function () {
-  console.log("Order submitted:");
-  console.log("Name:", this.name);
-  console.log("Email:", this.email);
-  console.log("Gender:", this.gender);
-  console.log("Payment method:", this.payment);
-  console.log("Delivery location:", this.location);
-  console.log("Ordered burgers (Proxy):", this.orderedBurgers);
-  
-  // Konvertera orderedBurgers objekt till array för orderItems
-  const orderItems = [];
-  for (const [burgerName, amount] of Object.entries(this.orderedBurgers)) {
-    for (let i = 0; i < amount; i++) {
-      orderItems.push(burgerName);
-    }
-  }
-  
-  socket.emit("addOrder", { 
-    orderId: this.getOrderNumber(),
-    details: { 
-      x: this.location.x,
-      y: this.location.y,
-       customerName: this.name,
-    customerEmail: this.email,
-    customerGender: this.gender,
-    paymentMethod: this.payment
+    getOrderNumber: function () {
+      return Math.floor(Math.random() * 100000);
     },
-    orderItems: orderItems 
-  });
-},
-  addToOrder: function (event) {
-    if (event.amount > 0) {
-      this.orderedBurgers[event.name] = event.amount;
-    } else {
-      delete this.orderedBurgers[event.name];
-    }
-    this.orderedBurgers = {...this.orderedBurgers};
+    addOrder: function (event) {
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+      this.location.x = event.clientX - 10 - offset.x;
+      this.location.y = event.clientY - 10 - offset.y;
+    },
+    submitOrder: function () {
+      console.log("Order submitted:");
+      console.log("Name:", this.name);
+      console.log("Email:", this.email);
+      console.log("Gender:", this.gender);
+      console.log("Payment method:", this.payment);
+      console.log("Delivery location:", this.location);
+      console.log("Ordered burgers (Proxy):", this.orderedBurgers);
+
+      // Konvertera orderedBurgers objekt till array för orderItems
+      const orderItems = [];
+      for (const [burgerName, amount] of Object.entries(this.orderedBurgers)) {
+        for (let i = 0; i < amount; i++) {
+          orderItems.push(burgerName);
+        }
+      }
+
+      socket.emit("addOrder", {
+        orderId: this.getOrderNumber(),
+        details: {
+          x: this.location.x,
+          y: this.location.y,
+          customerName: this.name,
+          customerEmail: this.email,
+          customerGender: this.gender,
+          paymentMethod: this.payment,
+        },
+        orderItems: orderItems,
+      });
+    },
+    addToOrder: function (event) {
+      if (event.amount > 0) {
+        this.orderedBurgers[event.name] = event.amount;
+      } else {
+        delete this.orderedBurgers[event.name];
+      }
+      this.orderedBurgers = { ...this.orderedBurgers };
+    },
+    setLocation: function (event) {
+      var offset = {
+        x: event.currentTarget.getBoundingClientRect().left,
+        y: event.currentTarget.getBoundingClientRect().top,
+      };
+
+      // Samma beräkning som i addOrder, men bara spara location
+      this.location.x = event.clientX - 10 - offset.x;
+      this.location.y = event.clientY - 10 - offset.y;
+
+      console.log("Location set to:", this.location.x, this.location.y);
+    },
   },
-  setLocation: function (event) {
-  var offset = {x: event.currentTarget.getBoundingClientRect().left,
-                y: event.currentTarget.getBoundingClientRect().top};
-  
-  // Samma beräkning som i addOrder, men bara spara location
-  this.location.x = event.clientX - 10 - offset.x;
-  this.location.y = event.clientY - 10 - offset.y;
-  
-  console.log("Location set to:", this.location.x, this.location.y);
-}
-  }
-}
+};
 </script>
 
 <style>
- #map-section {
-    margin: 20px;
-    border: 2px dashed;
-    padding: 20px;
+#map-section {
+  margin: 20px;
+  border: 2px dashed;
+  padding: 20px;
 }
 
 .map-container {
-    width: 500px;
-    height: 300px;
-    overflow: auto;
-    border: 1px solid #ccc;
-    position: relative;
+  width: 500px;
+  height: 300px;
+  overflow: auto;
+  border: 1px solid #ccc;
+  position: relative;
 }
 
 #map {
-    cursor: pointer;
+  cursor: pointer;
 }
 .target {
-    position: absolute;
-    background: red;
-    border-radius: 50%;
-    width: 20px;
-    height: 20px;
-    text-align: center;
-    line-height: 20px;
-    color: white;
-    font-weight: bold;
+  position: absolute;
+  background: red;
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  text-align: center;
+  line-height: 20px;
+  color: white;
+  font-weight: bold;
 }
 
-
-  @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap");
 body {
-    font-size: 12pt;
-    font-family: 'comic sans', sans-serif;
-}
-
-p {
-    color: black;
+  font-size: 12pt;
+  font-family: "comic sans", sans-serif;
 }
 
 h1 {
-    font-family: 'Times New Roman', Times, serif;
-    font-size: 36pt;
+  font-family: "Times New Roman", Times, serif;
+  font-size: 36pt;
 }
-main, header, footer, nav ul {
-    max-width: 80rem;
-    margin: 0 auto 0 auto;
+main,
+header,
+footer,
+nav ul {
+  max-width: 80rem;
+  margin: 0 auto 0 auto;
 }
 main {
-    background-color: bisque;
+  background-color: bisque;
 }
 
 /* nav ul li {
@@ -243,91 +305,91 @@ main {
 } */
 
 header h1 {
-    width:40rem;
-    margin: 0 auto;
-    text-align: center;
+  width: 40rem;
+  margin: 0 auto;
+  text-align: center;
 }
 
 nav ul {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, 9.25em);
-    gap: 1em;
-    padding: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 9.25em);
+  gap: 1em;
+  padding: 0;
 }
 
 nav li {
-    display: block;
-    background-color: grey;
-    padding: 1em;
+  display: block;
+  background-color: grey;
+  padding: 1em;
 }
 
 .Very-good {
-    color: green;
+  color: green;
 }
 
 .Master {
-    color: green;
-    font-weight: bold;
+  color: green;
+  font-weight: bold;
 }
 .allergen {
-    font-weight: bold;
-    text-transform: uppercase;
+  font-weight: bold;
+  text-transform: uppercase;
 }
 #burger-selection {
-    background-color: black;
-    color: white;
-    border-color: white;
+  background-color: black;
+  color: white;
+  border-color: white;
 }
 #customer-information {
-    border-color: black;
+  border-color: black;
+  color: black;
 }
 button:hover {
-    background-color: #1287b6; /* Mörkare grön när man hovrar */
-    cursor: pointer; /* Ändrar muspekaren till en hand */
+  background-color: #1287b6;
+  cursor: pointer;
 }
-section{
-    margin:20px;
-    border: 2px dashed;
-    padding:20px
+section {
+  margin: 20px;
+  border: 2px dashed;
+  padding: 20px;
 }
-button{
-    margin: 20px;
-    padding: 10px 20px;
+button {
+  margin: 20px;
+  padding: 10px 20px;
 }
 .burger {
-    padding: 10px;
+  padding: 10px;
 }
 header {
-    position: relative;
-    height: 300px;
-    overflow: hidden;
-    margin: 0 20px;
-    max-width: 80rem; /* SAMMA som dina andra element */
-    margin-left: auto; /* Centrera */
-    margin-right: auto; /* Centrera */
-
+  position: relative;
+  height: 300px;
+  overflow: hidden;
+  margin: 0 20px;
+  max-width: 80rem; /* SAMMA som dina andra element */
+  margin-left: auto; /* Centrera */
+  margin-right: auto; /* Centrera */
 }
 
 header img {
-    width: 100%;
-    height: auto;
-    opacity: 0.8; /* Justera opaciteten för att göra bilden lite genomskinlig */
+  width: 100%;
+  height: auto;
+  opacity: 0.8; /* Justera opaciteten för att göra bilden lite genomskinlig */
 }
 
 header h1 {
-    position: absolute;
-    top: 50%; /* Centrera vertikalt */
-    left: 50%; /* Centrera horisontellt */
-    transform: translate(-50%, -50%); /* Justera för exakt centrering */
-    margin: 0; /* Ta bort negativ margin */
-    padding: 20px;
-    color: white; /* Lägg till vit färg så den syns */
-    text-align: center;
-    width: 100%; /* Se till att den tar hela bredden */
+  position: absolute;
+  top: 50%; /* Centrera vertikalt */
+  left: 50%; /* Centrera horisontellt */
+  transform: translate(-50%, -50%); /* Justera för exakt centrering */
+  margin: 0; /* Ta bort negativ margin */
+  padding: 20px;
+  color: white; /* Lägg till vit färg så den syns */
+  text-align: center;
+  width: 100%; /* Se till att den tar hela bredden */
 }
 .burger-wrapper {
-    display: grid;
-    grid-gap: 5px;
-    grid-template-columns: 1fr 1fr 1fr; /* 3 kolumner, samma storlek */
+  display: grid;
+  grid-gap: 5px;
+  grid-template-columns: 1fr 1fr 1fr; /* 3 kolumner, samma storlek */
 }
 </style>
